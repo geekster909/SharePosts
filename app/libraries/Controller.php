0e036a1ae501ed302a3e0b822c1387ed2561410f
Justin Bond <jbond@schieferchopshop.com>
2018-05-04 13:26:17 -0700

    initial setup

---
+++ app/libraries/Controller.php
@@ -0,0 +1,27 @@
+<?php
+	/**
+	* Base Controller
+	* Loads the modesl and views
+	*/
+	class Controller
+	{
+		// Load model
+		public function model($model){
+			// Require model file
+			require_once '../app/models/'.$model.'.php';
+
+			// Instantiate model
+			return new $model;
+		}
+
+		// Load view
+		public function view($view, $data = []){
+			// require model file
+			if (file_exists('../app/views/'.$view.'.php')) {
+				require_once '../app/views/'.$view.'.php';
+			} else {
+				// View does not exist
+				die('View does not exist');
+			}
+		}
+	}
