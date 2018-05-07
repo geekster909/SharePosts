0b937273d6ffe6565c3617171e347b84bec1cc2f
Justin Bond <bond.justink@gmail.com>
2018-05-07 15:10:37 -0700

    created post edit functionality

---
+++ app/views/posts/edit.php
@@ -0,0 +1,21 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+	<a href="/posts" class="btn btn-light"><i class="fa fa-backward"></i> Back</a>
+	<div class="card card-body bg-light mt-5">
+		<h2>Edit Post</h2>
+		<form action="/posts/edit/<?php echo $data['id']; ?>" method="POST">
+			<div class="form-group">
+				<label for="title">Title: <sup>*</sup></label>
+				<input type="text" name="title" class="form-control form-control-lg <?php echo (!empty($data['title_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['title']; ?>" />
+				<span class="invalid-feedback"><?php echo $data['title_err']; ?></span>
+			</div>
+			<div class="form-group">
+				<label for="body">Body: <sup>*</sup></label>
+				<textarea name="body" class="form-control form-control-lg <?php echo (!empty($data['body_err'])) ? 'is-invalid' : ''; ?>"><?php echo $data['body']; ?></textarea>
+				<span class="invalid-feedback"><?php echo $data['body_err']; ?></span>
+			</div>
+			<input type="submit" value="Submit" class="btn btn-success">
+		</form>
+	</div>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
